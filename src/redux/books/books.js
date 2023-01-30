f5deebbd42647ae529e032abf257291775024455
Utsargo Roy <royutsargo@gmail.com>
2023-01-30 14:37:16 +0600

    Refactor src/redux/books/books.js and form.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,43 @@
 import { createReducer } from '@reduxjs/toolkit';
-
+import { v4 as uuidv4 } from 'uuid';
 // Types
 const ADD = 'bookstore-roy/books/ADD';
 const REMOVE = 'bookstore-roy/books/REMOVE';
 
 // Default sate
-const defaultState = [];
+const defaultState = [
+  {
+    id: uuidv4(),
+    title: 'Pale Blue Dot: A Vision of the Human Future in Space',
+    author: 'Carl Sagan',
+  },
+  {
+    id: uuidv4(),
+    title: 'A People\'s History of the United States',
+    author: 'Howard Zinn',
+  },
+];
 
 // Actions
 const addBook = (book) => ({
   type: ADD,
-  book,
+  payload: {
+    id: uuidv4(),
+    title: book.title,
+    author: book.author,
+  },
 });
 
-const removeBook = (book) => ({
+const removeBook = (index) => ({
   type: REMOVE,
-  book,
+  payload: index,
 });
 
 // Book Reducer
 
 const bookReducer = createReducer(defaultState, {
-  [ADD]: (state, { book }) => [...state, book],
-  [REMOVE]: (state, { book }) => state.filter((b) => b.id !== book.id),
+  [ADD]: (state, action) => [...state, action.payload],
+  [REMOVE]: (state, action) => [...state.filter((book) => book.id !== action.payload)],
 });
 
 export { addBook, removeBook };
